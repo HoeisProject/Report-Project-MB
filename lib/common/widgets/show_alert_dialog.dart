50195cd191e96e46ec4cdd53cd3dd4f0d87e0863
Christian P. W. H <christianpwhwork@gmail.com>
2023-03-24 18:28:04 +0700

    ver 0.0.2 (full intl, ntp DateFormater, and geoLocation)

---
+++ lib/common/widgets/show_alert_dialog.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+Future<void> showAlertDialog(
+    {required BuildContext context,
+    required IconData icon,
+    required String title,
+    required String content,
+    required String defaultActionText,
+    required VoidCallback? onButtonPressed}) async {
+  return await showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      icon: Icon(
+        icon,
+        size: 75.0,
+      ),
+      title: Text(title),
+      content: SizedBox(
+        height: 300.0,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Text(
+            content,
+            style: const TextStyle(fontSize: 14.0),
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        ElevatedButton(
+          style: Theme.of(context).elevatedButtonTheme.style,
+          onPressed: () => onButtonPressed!(),
+          child: SizedBox(
+            width: 75.0,
+            height: 30.0,
+            child: Center(
+              child: Text(
+                defaultActionText,
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
--- lib/feature_1/employee/screens/create_report.dart
+++ lib/feature_1/employee/screens/create_report.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:ntp/ntp.dart';
 import 'package:report_project/common/widgets/custom_button.dart';
 import 'package:report_project/common/widgets/input_text_field.dart';
+import 'package:report_project/common/widgets/show_alert_dialog.dart';
 import 'package:report_project/common/widgets/view_text_field.dart';
 import 'package:report_project/feature_1/employee/widgets/custom_appbar.dart';
 import 'package:report_project/feature_1/employee/widgets/report_attach_media.dart';
@@ -26,6 +31,78 @@ class ReportCreateState extends State<ReportCreate> {
   List<File?>? listMediaFile;
 
   bool? isLoading = false;
+
+  DateTime? _projectCreated;
+
+  Position? position;
+
+  String? locationAddress = 'Loading...';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getDateTimeLoc();
+  }
+
+  Future<void> _getDateTimeLoc() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return await showAlertDialog(
+        context: context,
+        icon: Icons.warning,
+        title: "Location Unavailable",
+        content: "Location services are disabled,\nplease enable it!",
+        defaultActionText: "CLOSE",
+        onButtonPressed: () {},
+      );
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return await showAlertDialog(
+          context: context,
+          icon: Icons.warning,
+          title: "Location Unavailable",
+          content: "Location permissions are denied\nplease approve it",
+          defaultActionText: "CLOSE",
+          onButtonPressed: () {},
+        );
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return await showAlertDialog(
+        context: context,
+        icon: Icons.warning,
+        title: "Location Unavailable",
+        content:
+            "Location permissions are denied permanently\nplease approve it",
+        defaultActionText: "CLOSE",
+        onButtonPressed: () {},
+      );
+    }
+
+    position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    List<Placemark> placeMarks =
+        await placemarkFromCoordinates(position!.latitude, position!.longitude);
+
+    Placemark place = placeMarks[0];
+
+    setState(() async {
+      _projectCreated = await NTP.now();
+
+      locationAddress =
+          '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,8 +128,9 @@ class ReportCreateState extends State<ReportCreate> {
           children: [
             inputTextField(context, keyProjectTitle, "Project Title",
                 projectTitleCtl, TextInputType.text, false, 1, (value) {}),
-            viewTextField(context, "Time and Date", ""),
-            viewTextField(context, "Location", ""),
+            viewTextField(context, "Time and Date",
+                DateFormat.yMMMEd().format(_projectCreated!)),
+            viewTextField(context, "Location", locationAddress!),
             inputTextField(context, keyProjectDesc, "Project Description",
                 projectDescCtl, TextInputType.text, false, 6, (value) {}),
             reportAttachMedia(context, "Attach Media", listMediaFile, () {}),
