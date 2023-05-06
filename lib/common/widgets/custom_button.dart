33f27a13504992eb70a2dbd8526533b6a36407a0
Christian P. W. H <christianpwhwork@gmail.com>
2023-05-06 19:25:55 +0700

    1.2.4+02(new icon app using flutter_launcher_icon)

--- lib/common/widgets/custom_button.dart
+++ lib/common/widgets/custom_button.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:report_project/common/styles/constant.dart';
 
-Widget customButton(BuildContext context, bool? isLoading, String labelText,
-    Color buttonColor, void Function()? onPressed) {
+Widget customButton(
+  BuildContext context,
+  bool? isLoading,
+  String labelText,
+  Color buttonColor,
+  void Function()? onPressed,
+) {
   return Container(
     margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
     child: Center(
